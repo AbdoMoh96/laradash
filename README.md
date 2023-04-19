b8e6286fa61f4945aa8d9f362daf10f2b3233960
armohamed <abdo.moh96@outlook.com>
2023-04-19 14:23:54 +0200

    inertiajs ready

---
+++ README.md
@@ -0,0 +1,6 @@
+<p align="center">LaraDash</p>
+
+
+## About LaraDash
+
+LaraDash is an awesome reactJs Dashboard for managing website content
---
+++ resources/React/Pages/Test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface propTypes {}
+
+const Test : React.FunctionComponent<propTypes> = () => {
+
+    return (
+        <div>
+            <h1>
+                welcome to Test Page
+            </h1>
+        </div>
+    )
+}
+
+export default Test;
