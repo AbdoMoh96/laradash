5fbd8c7c4113525ad64f9d7486db6d0857f1c786
Abdelrahman Mohammed <window.2015@hotmail.com>
2023-04-22 18:41:22 +0200

    working on adding eslint support

--- resources/React/Pages/HomePage/HomePage.tsx
+++ resources/React/Pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 interface propTypes {}
 
@@ -13,4 +13,4 @@ const HomePage : React.FunctionComponent<propTypes> = () => {
     )
 }
 
-export default HomePage;
+export default HomePage
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +8,6 @@ export default defineConfig({
             input: ['resources/React/app.tsx'],
             refresh: true,
         }),
+       eslintPlugin(),
     ],
 });
