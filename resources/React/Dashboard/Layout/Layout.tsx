5fff28c6339db1d0113b55b43a77d959b7b61fb3
Abdelrahman Mohammed <window.2015@hotmail.com>
2023-04-24 23:29:43 +0200

    fixed side menu

--- resources/React/Dashboard/Layout/Layout.tsx
+++ resources/React/Dashboard/Layout/Layout.tsx
@@ -19,17 +19,19 @@ const Layout : React.FunctionComponent<propTypes> = ({ children }) : JSX.Element
                <h1 style={{color: "white", width: "10vw"}}>Lorem ipsum dolor sit amet, consectetur adipisicin</h1>
             </aside>
 
+            <nav className={`top_nav ${sideMenuState ? 'top_nav_shrink' : ''}`}>
+                <img className='user_img' src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" alt=""/>
+
+                <button type='button' onClick={() => handelSideMenuStateChange()}>side menu</button>
+            </nav>
+
             <section className={`content_wrapper ${sideMenuState ? 'content_wrapper_shrink' : ''}`}>
-                <nav className='top_nav'>
-                    <img className='user_img' src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" alt=""/>
-
-                    <button type='button' onClick={() => handelSideMenuStateChange()}>side menu</button>
-                </nav>
-
-                <section className='main_content'>
-                    {children}
-                </section>
+               {children}
             </section>
+
+            <footer className={`main_footer ${sideMenuState ? 'main_footer_shrink' : ''}`}>
+                <h4>laradash @armohamed</h4>
+            </footer>
         </main>
     );
 };
--- resources/React/Dashboard/Pages/HomePage/HomePage.tsx
+++ resources/React/Dashboard/Pages/HomePage/HomePage.tsx
@@ -5,12 +5,20 @@ interface propTypes {}
 
 const HomePage : React.FunctionComponent<propTypes> = () => {
 
+    const prentTest  = () : Array<JSX.Element> => {
+        let elements = [];
+        for(let i = 0; i < 200; i++){
+            elements = [...elements, <h1 key={i}>
+                welcome to dashboard home page
+            </h1>];
+        }
+        return elements;
+    };
+
     return (
         <Layout>
             <div>
-                <h1>
-                    welcome to dashboard home page
-                </h1>
+                {prentTest()}
             </div>
         </Layout>
     );
